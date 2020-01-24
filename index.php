54977ee233fdea8d6d784ac85384d94416d5d651
Luis Dario <luisda13@yahoo.es>
2020-01-23 21:38:34 -0600

    CAMBIOS

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php require_once("index.html")?>
+<?php 
+
+require_once("index.html")
+require_once("drama.html")
+require_once("404.html")
+require_once("accion.html")
+require_once("historia.html")
+require_once("lista.html")
+require_once("login.html")
+require_once("nosotros.html")
+require_once("noticias.html")
+require_once("proximamente.html")
+require_once("registro.html")
+require_once("series.html")
+require_once("terror.html")
+
+
+?>
